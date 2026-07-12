9e0799171a0bbc9fddcd2bf01c70b07d6718b3a5
Lagula Lakshmi Prasanna <prasannalagula@gmail.com>
2026-07-12 21:20:12 +0530

    Day-10-Employee Management SQL Practice

--- Projects/Employee Management System/employee_management.sql
+++ Projects/Employee Management System/employee_management.sql
@@ -119,3 +119,137 @@ delete from employee
 where age<20;
 -- "Add a new employee."
 INSERT into employee values(106,'Ravi',24,35000,'2024-06-15',3);
+-- ===========================================
+-- Day 09 - Employee Management
+-- Name : Lakshmi Prasanna
+-- Date : 12-07-2026
+-- ===========================================
+select*from employee;
+select*from department;
+
+-- Display the employee name, department name, and salary of employees whose salary is greater than the company average salary.
+select name,dept_name,salary
+from employee
+inner join department
+on employee.dept_id=department.dept_id
+where salary >
+(
+	select Avg(salary) 
+  	from employee
+  	
+);
+
+-- Display the department name and the total number of employees working in each department.
+select dept_name,count(employee.e_id) as Total_employees
+from employee
+right join department
+on employee.dept_id=department.dept_id
+group by dept_name;
+
+-- Display the department name having the highest total salary expense.
+select dept_name
+from employee
+inner join department
+on employee.dept_id=department.dept_id
+group by dept_name
+having SUM(salary)=
+(
+	select max(total_salary)
+  	from
+  	(
+		select SUM(salary) as total_salary
+  		from employee
+  		group by dept_id
+    )AS temp
+);
+
+-- Display employees who are older than every employee in the HR department.
+select name
+from employee
+where age>
+(
+	select max(age)
+	from employee
+	where dept_id='1'
+);  
+
+-- Display employees who joined in the same year as Rahul.
+select name from employee
+where year(joining_date)=
+(
+	select year(joining_date)
+  	from employee
+    where name='Rahul'
+);
+
+-- Display employees whose salary is greater than Asha but less than Aishu.
+select name
+from employee
+where salary > 
+(
+	select salary 
+	from employee
+  	where name='Asha'
+)  
+And salary <
+(
+  	select salary
+	from employee
+  	where name='Aishu'
+);
+
+-- Display Department Name,Highest Salary,Lowest Salary,Average Salary for every department.
+select dept_name,MAX(salary),MIN(salary),Avg(salary)
+from employee
+inner join department
+on employee.dept_id=department.dept_id
+group by dept_name;
+
+-- Find departments having exactly 2 employees.
+select dept_name,count(*)
+from employee
+inner join department
+on employee.dept_id=department.dept_id
+group by dept_name
+having count(*)=2;
+
+-- Display the second youngest employee.
+select age,name 
+from employee
+where age=
+(
+	select min(age)
+	from employee
+	where age>
+	(
+		select min(age)
+		from employee
+     ) 
+);
+
+-- Display employees earning the second highest salary in their own department.
+SELECT e1.name, e1.salary, e1.dept_id
+FROM employee e1
+WHERE e1.salary =
+(
+    SELECT MAX(e2.salary)
+    FROM employee e2
+    WHERE e2.dept_id = e1.dept_id
+      AND e2.salary <
+      (
+          SELECT MAX(e3.salary)
+          FROM employee e3
+          WHERE e3.dept_id = e1.dept_id
+      )
+);
+
+-- Increase the salary of employees who earn less than the company average salary by 5%.
+update employee
+set salary=salary*1.05
+where salary <
+    (
+		select AVG(salary) 
+		from employee
+  	);
+  
+  
